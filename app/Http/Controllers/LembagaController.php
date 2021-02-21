0877824531406216f14f32c7c3956aa97fc22add
hafet17 <do.crazy192@gmail.com>
2021-02-21 23:00:02 +0700

    create LembagaController

---
+++ app/Http/Controllers/LembagaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Lembaga;
+use Illuminate\Http\Request;
+
+class LembagaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Lembaga  $lembaga
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lembaga $lembaga)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Lembaga  $lembaga
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lembaga $lembaga)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Lembaga  $lembaga
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lembaga $lembaga)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Lembaga  $lembaga
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lembaga $lembaga)
+    {
+        //
+    }
+}
