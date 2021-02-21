d539eb69cb0214a7d1cdf98c333fa86d4f21bf25
hafet17 <do.crazy192@gmail.com>
2021-02-21 23:01:02 +0700

    modifikasi user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('last_name');
+
+            $table->string('nama_lembaga');
+            $table->text('alamat');
+            $table->string('no_telp');
             $table->string('email')->unique();
+            $table->string('nama_pimpinan');
+            $table->date('tahun_berdiri');
+            $table->string('susunan_pengurus');
+            $table->string('nama_pendiri');
+            $table->integer('jumlah_guru');
+            $table->integer('jumlah_santri');
+            $table->string('tempat_kbm');
+            $table->string('jadwal_kegiatan')->nullable();
+            $table->string('foto_kegiatan')->nullable();
+            $table->string('link_fb')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
