b1d4660a03b541e316f735425626bad4bbb4c340
hafet17 <do.crazy192@gmail.com>
2021-02-23 10:36:45 +0700

    add link new  sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -76,6 +76,23 @@
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
 
+    <!-- Pendaftar -->
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Pendaftar</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Jenis Lembaga</h6>
+                <a class="collapse-item" href="{{ route('pendaftar.tpa') }}">TPA</a>
+                <a class="collapse-item" href="{{ route('pendaftar.tpq') }}">TPQ</a>
+                <a class="collapse-item" href="{{ route('pendaftar.diniyah') }}">DINIYAH</a>
+            </div>
+        </div>
+    </li>
+
     <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
