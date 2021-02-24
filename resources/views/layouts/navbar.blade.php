c9f334946c8555e8e4d2431e95fcee82897ca174
hafet17 <do.crazy192@gmail.com>
2021-02-24 07:16:37 +0700

    add gate roles in navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,7 +4,8 @@
 
     <div class="topbar-divider d-none d-sm-block"></div>
 
-    <!-- Nav Item - User Information -->
+    <!-- Nav User -->
+    @can('isUser')
     <li class="nav-item dropdown no-arrow">
         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
             aria-haspopup="true" aria-expanded="false">
@@ -33,5 +34,25 @@
             </a>
         </div>
     </li>
+    @elsecan('isAdmin')
+    <!-- Nav Admin -->
+    <li class="nav-item dropdown no-arrow">
+        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
+            <figure class="img-profile rounded-circle avatar font-weight-bold bg-success"
+                data-initial="{{ Auth::user()->name[0] }}"></figure>
+        </a>
+        <!-- Dropdown - User Information -->
+        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+            <a class="dropdown-item" href="{{ route('admin.profil') }}">
+                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                {{ __('Profile') }}
+            </a>
+        </div>
+    </li>
+    @endcan
+
+
 
 </ul>
