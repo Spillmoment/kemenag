c001a6d32a8f89a63f98b2d1d6ab9ffc4b113909
hafet17 <do.crazy192@gmail.com>
2021-02-24 12:33:25 +0700

    refactor parent admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,23 +5,15 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Laravel SB Admin 2">
-    <meta name="author" content="Alejandro RH">
+    <meta name="description" content="TPA TPQ MADIN KEMENAG">
+    <meta name="author" content="TPA TPQ MADIN KEMENAG">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-
+    @include('layouts.style')
     <!-- Favicon -->
     <link href="{{ asset('img/favicon.png') }}" rel="icon" type="image/png">
 </head>
@@ -73,9 +65,9 @@
             <!-- End of Footer -->
 
             @include('layouts.script')
-            @stack('prepend-script')
-            <script src="{{ asset('vendor/datatables/js/jquery.dataTables.min.js') }}"></script>
-            <script src="{{ asset('vendor/datatables/js/dataTables.bootstrap5.min.js') }}"></script>
+            <!-- Page level plugins -->
+            <script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
+            <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
             <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
             @stack('scripts')
 
@@ -95,14 +87,14 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">{{ __('Ready to Leave?') }}</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">{{ __('Siap untuk keluar?') }}</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-body">Yakin Anda ingin keluar</div>
                 <div class="modal-footer">
-                    <button class="btn btn-link" type="button" data-dismiss="modal">{{ __('Cancel') }}</button>
+                    <button class="btn btn-link" type="button" data-dismiss="modal">{{ __('Batal') }}</button>
                     <a class="btn btn-danger" href="{{ route('logout') }}"
                         onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
