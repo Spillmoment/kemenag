2c83e59a27d1a2e1513557d6cf837e613b5adaf8
hafet17 <do.crazy192@gmail.com>
2021-02-25 13:52:46 +0700

    add style bg image

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -13,7 +13,9 @@
 
     <!-- Fonts -->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
@@ -23,6 +25,12 @@
 </head>
 
 <body class="bg-gradient-success min-vh-100 d-flex justify-content-center align-items-center">
+    <style>
+        .bg-image {
+            background-position: center;
+            background-size: cover;
+        }
+    </style>
 
     @yield('main-content')
 
