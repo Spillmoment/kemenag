30d7049c119a8631b8866505371c3a783cdccc34
hafet17 <do.crazy192@gmail.com>
2021-02-25 19:04:34 +0700

    add script gijgo cdn

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -5,3 +5,4 @@
 <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
 <!-- Page level custom scripts -->
 <script src="{{ asset('js/demo/datatables-demo.js') }}"></script>
+<script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
