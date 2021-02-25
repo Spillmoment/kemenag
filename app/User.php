5ee017d40106a4cb9fbc84946bdf157dd48ccc62
hafet17 <do.crazy192@gmail.com>
2021-02-25 19:02:14 +0700

    rapikan model user

--- app/User.php
+++ app/User.php
@@ -10,46 +10,17 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password', 'status'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    /**
-     * Get the user's full name.
-     *
-     * @return string
-     */
-    // public function getFullNameAttribute()
-    // {
-    //     if (is_null($this->last_name)) {
-    //         return "{$this->name}";
-    //     }
-
-    //     return "{$this->name} {$this->last_name}";
-    // }
 
     public function setPasswordAttribute($value)
     {
