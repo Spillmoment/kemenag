461cd95f1867d1dbae1863ae86ce602c6cb5e78e
hafet17 <do.crazy192@gmail.com>
2021-02-27 16:24:51 +0700

    refactor dashboard admin

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -37,7 +37,11 @@
                         <i class="fas fa-users fa-2x text-gray-300"></i>
                     </div>
                 </div>
+                <div class="mt-3 font-weight-900 text-primary"> <a href="{{ route('pendaftar.baru') }}">
+                        Selengkapnya </a><i class=" fas fa-arrow-right"></i>
+                </div>
             </div>
+
         </div>
     </div>
 
@@ -47,12 +51,41 @@
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">TPA</div>
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">TPA/TPQ</div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $widget['tpa'] }}</div>
                     </div>
                     <div class="col-auto">
-                        <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                        <i class="fas fa-user fa-2x text-gray-300"></i>
                     </div>
+                </div>
+                <div class="mt-3 font-weight-900 text-success"> <a href="{{ route('pendaftar.tpa') }}"
+                        class="text-success">
+                        Selengkapnya </a><i class=" fas fa-arrow-right"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-warning shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Madin</div>
+                        <div class="row no-gutters align-items-center">
+                            <div class="col-auto">
+                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $widget['madin'] }}</div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-user fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+                <div class="mt-3 font-weight-900 text-warning"> <a href="{{ route('pendaftar.madin') }}"
+                        class="text-warning">
+                        Selengkapnya </a><i class=" fas fa-arrow-right"></i>
                 </div>
             </div>
         </div>
@@ -64,34 +97,21 @@
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">TPQ</div>
+                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Majelis Taklim</div>
                         <div class="row no-gutters align-items-center">
                             <div class="col-auto">
-                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $widget['tpq'] }}</div>
+                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $widget['majlis'] }}</div>
                             </div>
 
                         </div>
                     </div>
                     <div class="col-auto">
-                        <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                        <i class="fas fa-user fa-2x text-gray-300"></i>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Users -->
-    <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-warning shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Madin</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $widget['madin'] }}</div>
-                    </div>
-                    <div class="col-auto">
-                        <i class="fas fa-book fa-2x text-gray-300"></i>
-                    </div>
+                <div class="mt-3 font-weight-900 text-info"> <a href="{{ route('pendaftar.majlis') }}"
+                        class="text-info">
+                        Selengkapnya </a><i class=" fas fa-arrow-right"></i>
                 </div>
             </div>
         </div>
